3e794a0f15e34a0a7cbb702b157f6195d9ce852c
Bessel Raissi <bessel.raissi@edu.isetcom.tn>
2026-03-15 17:31:44 +0100

    Initialisation projet

---
+++ profil.php
@@ -0,0 +1,29 @@
+<?php
+$nom = "Raissi";
+$prenom = "Bessel";
+$email = "bessel.raissi@edu.isetcom.tn";
+$age = 20;
+$ville = "Tunis";
+$formation = "GTIC";
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>Profil utilisateur</title>
+</head>
+<body>
+    <h1>Profil utilisateur</h1>
+    <p><strong>Nom :</strong> <?php echo $nom; ?></p>
+    <p><strong>Prénom :</strong> <?php echo $prenom; ?></p>
+    <p><strong>Email :</strong> <?php echo $email; ?></p>
+    <p><strong>Âge :</strong> <?php echo $age; ?> ans</p>
+    <p><strong>Ville :</strong> <?php echo $ville; ?></p>
+    <p><strong>Formation :</strong> <?php echo $formation; ?></p>
+    <?php echo "<p>Bienvenue $prenom dans la formation $formation</p>"; ?>
+</body>
+</html>
+
+<p>Date : <?= date("H:i:s") ?></p>
